03f92c47f4a672ea8871258e7d924782047b6e69
atzina <atzina@gmail.com>
2023-05-17 13:22:14 -0600

    ultimos

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,13 @@
-
-import Image from 'next/image'
-import styles from './page.module.css'
-
-'use client'; // This is a client component
+'use client'
 import styles from './page.module.css';
 import AllProducts from './componets/allProducts';
-
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <hi>navbar</hi>
 
-  
-      <h1>Lista de todos los productos</h1>
-
-   
-
-
-
       <AllProducts />
     </main>
   );
-
 }
